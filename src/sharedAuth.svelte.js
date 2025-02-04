1d73021dfa7db8b038c7d27112bd02555674f6fc
Elmer <elmer.go@outlook.com>
2025-02-04 01:17:22 -0400

    Add shared auth status for all components

---
+++ src/sharedAuth.svelte.js
@@ -0,0 +1,26 @@
+import { supabase } from "./supabaseClient";
+
+export const user = $state({
+    name: null,
+    isAnon: null,
+    isAuthenticated: false
+});
+
+export async function getCurrentUser() {
+    const { data, error } = await supabase.auth.getUser();
+
+    if (data.user === null) {
+        user.name = "Anonymous User";
+        user.isAnon = true;
+    } else {
+        user.name = data.user.email;
+        user.isAnon = false;
+        user.isAuthenticated = true;
+    }
+}
+
+export async function signOut() {
+    const { error } = await supabase.auth.signOut();
+
+    getCurrentUser();
+}
