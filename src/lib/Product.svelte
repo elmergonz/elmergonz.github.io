e62710ec6136768b2926c38280fd5a42e9f89026
Elmer <elmer.go@outlook.com>
2025-02-04 01:43:57 -0400

    Move individual products here

---
+++ src/lib/Product.svelte
@@ -0,0 +1,14 @@
+<script>
+    let { product, user } = $props();
+</script>
+
+<li id={`product-${product.id}`}>
+    {product.name}
+    {#if !user.isAnon}
+        <input
+            type="button"
+            value="delete"
+            onclick={deleteProduct(product.id)}
+        />
+    {/if}
+</li>
