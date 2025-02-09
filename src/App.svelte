f2fb7a09a749659fb21cf9b7b41d624883fd38c2
Elmer <elmer.go@outlook.com>
2025-02-09 03:27:04 -0400

    Integrate Valentine component in app

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
     import { user, getCurrentUser } from "./sharedAuth.svelte";
     import Auth from "./lib/Login.svelte";
     import Catalog from "./lib/Catalog.svelte";
+    import Valentine from "./lib/Valentine.svelte";
 
     onMount(() => {
         getCurrentUser();
@@ -12,6 +13,8 @@
 <main>
     {#if user.isAuthenticated}
         <Catalog />
+    {:else if user.isValentine}
+        <Valentine />
     {:else}
         <Auth />
     {/if}
